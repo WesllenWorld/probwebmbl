762a5ca6691d95e4612a08e307484a62b67a4cc5
WesllenWorld <wesllen.2020111052@unicap.br>
2022-03-31 23:25:28 -0300

    1 file modified

--- ex02/script.js
+++ ex02/script.js
@@ -1,104 +1,39 @@
-/* Aula 18 de JS Conversor de Moedas  */
+let dolares = 0;
+let reais = 0;
 
-/* SELECIONAR ELEMENTOS */
-// selecionar input com o numero digitado
-let valorDigitado = document.querySelector("valorBruto")
-let botaoTaxa = document.querySelector("botaoTaxa")
-let botaoTaxaLimpar = document.querySelector("botaoTaxaLimpar")
-let botaoDolarReal = document.querySelector("botaoDolarReal")
-let botaoRealDolar = document.querySelector("botaoRealDolar")
+let taxaDeConversao = document.getElementById("taxa");
+let valorDolar = document.getElementById("valorDolar");
+let valorReal = document.getElementById("valorReal");
 
 
-valorDigitado.addEventListener("change", botaoTaxa);
-/* VERIFICAR SE FOI DIGITADO ALGUM VALOR PARA PODER CONVERTER */
-function botaoTaxa() {
-  if(valorDigitado.value == 0 || valorDigitado == '' || valorDigitado == null) {
-      botaoTaxa.disabled = true;
-      btnConverter.style.background = '#ccc'
-      btnConverter.style.cursor = 'not-allowed'
-  }
+
+let botaoTaxa = document.getElementById("botaoTaxa")
+let botaoTaxaLimpar = document.getElementById("botaoTaxaLimpar")
+let botaoDolarReal = document.getElementById("botaoDolarReal")
+let botaoRealDolar = document.getElementById("botaoRealDolar")
+
+botaoDolarReal.disabled = true;
+botaoRealDolar.disabled = true;
+
+
+function dolarReal(){
+  reais = taxaDeConversao.value * valorDolar;
+  valorReal.value = reais;
 }
-
-// REATIVAR BOTAO
-function botaoTaxaLimpar() {
-  if(valorDigitado.value > 0) {
-    botaoTaxa.disabled = false;
-      btnConverter.style.background = '#ffc107'
-      btnConverter.style.cursor = 'pointer'
-  } else {
-      console.log('Nao ativou')
-  }
-}
-
-let aviso = document.querySelector('#aviso')
-
-
-
-// MENSAGEM FORMATADA PARA EXIBIR VALORES MONETARIOS
-function mensagemFormatada(moedaConvertida) {
-    isNaN(valorEmReal) ? valorEmReal = 0 : ''
-    console.log("Moeda Convertida " + moedaConvertida)
-    aviso.textContent = "O valor " + (valorEmReal).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) + " convertido em " + moedaEstrangeira + " é " + moedaConvertida
+function realDolar(){
+  dolares = valorReal/taxaDeConversao.value;
+  valorReal.value = dolares;
 }
 
 
+function taxaLimpar() {
+  document.getElementsByName("real")[0].value = "";
+  document.getElementsByName("taxa")[0].value = "";
+  document.getElementsByName("dolar")[0].value = "";
 
-// VERIFICAR QUAL BOTAO RADIO ESTA MARCADO checked ou checked == true
-// vincular a verificacao a um evento, click no botao Converter
-btnConverter.addEventListener('click', function() {
-    // FAZER o parseFloat dos valores monetarios (converter String para Float)
-    valorEmReal = parseFloat(valorDigitado.value)
+  document.getElementById("botaoTaxa").disabled = false;
+ 
+  botaoRealDolar.disabled = true;
+  botaoDolarReal.disabled = true;
 
-    console.log('Escolhe a moeda estrangeira')
-    for(let i = 0; i < moedaSelecionada.length; i++) {
-        if(moedaSelecionada[i].checked) {
-            moedaEstrangeira = moedaSelecionada[i].value
-            console.log(moedaEstrangeira)
-        }
-    }
-
-    /*
-    Use uma estrutura escolha caso para escolher
-    qual e a moeda estrangeira que foi selecionada
-    */
-
-// {moedaConvertida.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
-// CONVERSAO DE MOEDAS
-// Operacao basica pegar moedaEstrangeira e dividir pelo valorEmReal
-    switch(moedaEstrangeira) {
-        
-        case 'Dólar':
-            moedaConvertida = valorEmReal / valorDoDolar
-            mensagemFormatada(moedaConvertida.toLocaleString('en-US', { style: 'currency', currency: 'USD' }))
-        break
-
-        case 'Euro':
-            moedaConvertida = valorEmReal / valorDoEuro
-            mensagemFormatada(moedaConvertida.toLocaleString('de-DE', { style: 'currency', currency: 'EUR' }))
-        break
-
-        case 'Libra':
-            moedaConvertida = valorEmReal / valorDaLibra
-            mensagemFormatada(moedaConvertida.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' }))
-        break
-
-        case 'Bitcoins':
-            moedaConvertida = valorEmReal / valorDoBitcoin
-            mensagemFormatada(parseFloat(moedaConvertida).toFixed(5))
-        break
-    
-        default:
-            aviso.textContent = 'Escolha uma moeda estrangeira'
-    }
-    isNaN(moedaConvertida) ? moedaConvertida = 0 : ''
-})
-
-btnLimpar.addEventListener('click', function() {
-    valorDigitado.focus()
-    valorDigitado.value = ''
-    aviso.textContent = 'Digite o valor, escolha a moeda e converter'
-    moedaSelecionada[0].checked = false
-    moedaSelecionada[1].checked = false
-    moedaSelecionada[2].checked = false
-    moedaSelecionada[3].checked = false
-})
+}
