e6c734c4865b849d2a992a46db912206ebaa8dd2
WesllenWorld <wesllen.2020111052@unicap.br>
2022-04-08 18:05:30 -0300

    2 files modified

--- Contador - Geração automática/script.js
+++ Contador - Geração automática/script.js
@@ -3,13 +3,25 @@ let cont2 = 0;
 let d1 = document.getElementById("contador1");
 let d2 = document.getElementById("contador2");
 
-function reset(){
-  cont = 0;
+
+function reset(div){
+  if(div === "div1"){
+    cont = 0;
   printar(cont);
+  }else{
+    cont2 = 0;
+   printar(cont);
+  }
+ 
 }
 
-function somaCont(){
-  printar(cont++);
+function somaCont(div){
+  if(div === "div1"){
+    printar(cont++);
+  }else{
+    printar(cont2++);
+  }
+
 }
 
 function printar(variavel) {
