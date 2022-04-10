849c154200b4d76452417b05d780a26e6fa5bd87
WesllenWorld <wesllen.2020111052@unicap.br>
2022-04-09 22:35:19 -0300

    1 file modified

--- Contador - Geração automática/script.js
+++ Contador - Geração automática/script.js
@@ -43,7 +43,7 @@ function contadorNovo() {
     // Set the button text to 'Can you click me?'
     button.innerText = 'Can you click me?'
     button.addEventListener("click", ()=>{
-      c.innerHTML(c++);
+      c.nodeValue(c++);
     });
     
 
