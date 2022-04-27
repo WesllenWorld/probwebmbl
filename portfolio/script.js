96014333d93a443433853f2d476c2b1906040a8a
WesllenWorld <wesllen.2020111052@unicap.br>
2022-04-26 22:05:42 -0300

    2 files modified

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,5 +1,5 @@
 let btn = document.getElementById("mostrar-aprendendo");
-let div = document.getElementById("divAprendendo");
+let div = document.getElementById("div-aprendendo");
 
 btn.addEventListener("click", () => {
   div.classList.toggle('hide');
