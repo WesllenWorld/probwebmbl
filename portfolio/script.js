6ac2a8f139c1daf1c6011c1731e903e3354f836f
WesllenWorld <wesllen.2020111052@unicap.br>
2022-04-26 22:24:29 -0300

    2 files modified

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,5 +2,9 @@ let btn = document.getElementById("mostrar-aprendendo");
 let div = document.getElementById("div-aprendendo");
 
 btn.addEventListener("click", () => {
-  div.classList.toggle('hide');
+  if (div.style.display !== "none") {
+    div.style.display = "none";
+  } else {
+    div.style.display = "block";
+  }
 })
