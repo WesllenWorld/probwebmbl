ab29549135532061cc92961626ca29a607eab78a
WesllenWorld <wesllen.2020111052@unicap.br>
2022-05-03 18:08:43 -0300

    1 file modified

--- ATIVIDADE_PROJETO/script.js
+++ ATIVIDADE_PROJETO/script.js
@@ -24,6 +24,7 @@ xhr.withCredentials = false;
 
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
+		var p = this.responseText;
 		console.log(this.responseText);
 	}
 });
@@ -35,7 +36,7 @@ xhr.setRequestHeader("X-RapidAPI-Key", "9c2551b869mshc8173a28e9ebecdp14e04bjsne4
 xhr.send(data);
 
 	const paragrafo = document.createElement("p");
-	const texto = document.createTextNode(responseText);
+	const texto = document.createTextNode(p);
 	paragrafo.appendChild(texto);
 	const element = document.getElementById("div1");
 	element.appendChild(paragrafo);
