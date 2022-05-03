8ae1a146919e3412b57b7ff05bfbdb960322c064
WesllenWorld <wesllen.2020111052@unicap.br>
2022-05-02 22:08:56 -0300

    1 file modified

--- ATIVIDADE_PROJETO/script.js
+++ ATIVIDADE_PROJETO/script.js
@@ -1,7 +1,7 @@
 const data = null;
 
 const xhr = new XMLHttpRequest();
-xhr.withCredentials = false;
+xhr.withCredentials = true;
 
 xhr.addEventListener("readystatechange", function () {
 	if (this.readyState === this.DONE) {
@@ -9,8 +9,8 @@ xhr.addEventListener("readystatechange", function () {
 	}
 });
 
-xhr.open("GET", "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe?query=soup&offset=1");
-xhr.setRequestHeader("X-RapidAPI-Host", "recipe-by-api-ninjas.p.rapidapi.com");
+xhr.open("GET", "https://random-recipes.p.rapidapi.com/ai-quotes/0");
+xhr.setRequestHeader("X-RapidAPI-Host", "random-recipes.p.rapidapi.com");
 xhr.setRequestHeader("X-RapidAPI-Key", "9c2551b869mshc8173a28e9ebecdp14e04bjsne4e4b3f756f5");
 
 xhr.send(data);
